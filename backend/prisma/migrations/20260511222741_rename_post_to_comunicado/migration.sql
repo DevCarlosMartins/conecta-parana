bb4ede5e668e8dcaf258642551260e0931a1f21f
PedroAzedo <pedroaugustus0704@gmail.com>
2026-05-13 03:28:39 -0300

    refactor: migrate post_id to comunicado_id preserving existing data

--- backend/prisma/migrations/20260511222741_rename_post_to_comunicado/migration.sql
+++ backend/prisma/migrations/20260511222741_rename_post_to_comunicado/migration.sql
@@ -1,13 +1,14 @@
-/*
-  Warnings:
+CREATE TABLE "comunicados" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "is_active" BOOLEAN NOT NULL,
 
-  - You are about to alter the column `coordinates` on the `events` table. The data in that column could be lost. The data in that column will be cast from `Text` to `Unsupported("geometry(Point, 4326)")`.
-  - You are about to drop the column `post_id` on the `favorites` table. All the data in the column will be lost.
-  - You are about to drop the column `post_id` on the `likes` table. All the data in the column will be lost.
-  - You are about to drop the column `post_id` on the `notifications` table. All the data in the column will be lost.
-  - You are about to drop the `posts` table. If the table is not empty, all the data it contains will be lost.
+    CONSTRAINT "comunicados_pkey" PRIMARY KEY ("id")
+);
 
-*/
+
+
 -- DropForeignKey
 ALTER TABLE "favorites" DROP CONSTRAINT "favorites_post_id_fkey";
 
@@ -22,41 +23,31 @@ ALTER TABLE "events" ALTER COLUMN "coordinates" DROP NOT NULL,
 ALTER COLUMN "coordinates" SET DATA TYPE geometry(Point, 4326);
 
 -- AlterTable
-ALTER TABLE "favorites" DROP COLUMN "post_id",
-ADD COLUMN     "comunicado_id" INTEGER;
+ALTER TABLE "favorites" ADD column comunicado_id INTEGER;
+UPDATE "favorites" SET comunicado_id = post_id;
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_comunicado_id_fkey" 
+FOREIGN KEY ("comunicado_id") REFERENCES "comunicados"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "favorites" DROP COLUMN "post_id";
 
 -- AlterTable
-ALTER TABLE "likes" DROP COLUMN "post_id",
-ADD COLUMN     "comunicado_id" INTEGER;
+ALTER TABLE "likes" ADD COLUMN "comunicado_id" INTEGER;
+UPDATE "likes" SET comunicado_id = post_id;
+ALTER TABLE "likes" ADD CONSTRAINT "likes_comunicado_id_fkey" 
+FOREIGN KEY ("comunicado_id") REFERENCES "comunicados"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "likes" DROP COLUMN "post_id";
 
 -- AlterTable
-ALTER TABLE "notifications" DROP COLUMN "post_id",
-ADD COLUMN     "comunicado_id" INTEGER;
+ALTER TABLE "notifications" ADD COLUMN "comunicado_id" INTEGER;
+UPDATE "notifications" SET comunicado_id = post_id;
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_comunicado_id_fkey" 
+FOREIGN KEY ("comunicado_id") REFERENCES "comunicados"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "notifications" DROP COLUMN "post_id";
 
 -- DropTable
 DROP TABLE "posts";
-
--- CreateTable
-CREATE TABLE "comunicados" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "is_active" BOOLEAN NOT NULL,
-
-    CONSTRAINT "comunicados_pkey" PRIMARY KEY ("id")
-);
 
 -- CreateIndex
 CREATE INDEX "events_coordinates_idx" ON "events" USING GIST ("coordinates");
 
 -- CreateIndex
 CREATE INDEX "locals_coordinates_idx" ON "locals" USING GIST ("coordinates");
-
--- AddForeignKey
-ALTER TABLE "likes" ADD CONSTRAINT "likes_comunicado_id_fkey" FOREIGN KEY ("comunicado_id") REFERENCES "comunicados"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "favorites" ADD CONSTRAINT "favorites_comunicado_id_fkey" FOREIGN KEY ("comunicado_id") REFERENCES "comunicados"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "notifications" ADD CONSTRAINT "notifications_comunicado_id_fkey" FOREIGN KEY ("comunicado_id") REFERENCES "comunicados"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
